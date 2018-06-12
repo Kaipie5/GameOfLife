68fd537d0dfda68217b0eb8779608ed6e289cb45
Kai <mcconnka@whitman.edu>
2018-06-12 09:55:55 -0700

    Added Constants for the Game

---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,20 @@
+
+package gameoflife;
+
+/**
+ *
+ * @author kaimcconnell
+ */
+public class GameOfLife {
+
+    
+    //Constants
+    static int BOARDHEIGHT = 20;
+    static int BOARDWIDTH = 20;
+    static double STARTWEIGHT = 0.30;
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

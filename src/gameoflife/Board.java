608b898d2fffdf3ab4e016e83cd330c33a33d1af
Kai <mcconnka@whitman.edu>
2018-06-13 09:52:21 -0700

    state change methods built in board class

--- src/gameoflife/Board.java
+++ src/gameoflife/Board.java
@@ -4,28 +4,44 @@
  * and open the template in the editor.
  */
 package gameoflife;
-
-import java.util.ArrayList;
+import static gameoflife.GameOfLife.*;
 
 /**
  *
  * @author kaimcconnell
  */
 public class Board {
-    private ArrayList<Cell> cells;
-    private int height;
-    private int width;
+    private Cell[][] cells;
+    private final int height;
+    private final int width;
     
-    public Board(int height, int width) {
+    public Board(int height, int width, double lifeLikelihoodForCell) {
         this.height = height;
         this.width = width;
+        cells = new Cell[height][width];
+        
+        for (int row = 0; row < height; row++){
+            
+            for (int col = 0; col < width; col++) {
+                
+                Coordinate c = new Coordinate(row, col);
+                Cell newCell = new Cell(c);
+                
+                double rand = Math.random();
+                
+                if (rand <= lifeLikelihoodForCell) {
+                    newCell.bringToLife();
+                }
+                cells[row][col] = newCell;
+            }
+        }
     }
 
-    public ArrayList<Cell> getCells() {
+    public Cell[][] getCells() {
         return cells;
     }
 
-    public void setCells(ArrayList<Cell> cells) {
+    public void setCells(Cell[][] cells) {
         this.cells = cells;
     }
 
@@ -33,31 +49,53 @@ public class Board {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public int getWidth() {
         return width;
     }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
     
-    public void initBoard() {
+    
+    private int calculateAliveNeighborsOfCurrentCell(int rowIndex, int colIndex) {
+        int numAlive = 0;
+        for (int row = rowIndex - 1; row <= rowIndex + 1; row++) {
+            
+            for (int col = colIndex - 1; col <= colIndex + 1; col++) {
+                
+                if (row == rowIndex && col == colIndex) continue;
+                
+                if (row >= 0 && col >=0 && row < height && col < width) {
+                    if (cells[row][col].isAlive()) {
+                        numAlive++;
+                    }
+                }  
+            }
+        }
+        return numAlive;
+    }
+    private boolean calculateNextStateOfCell(int row, int col) {
+        int numAlive = calculateAliveNeighborsOfCurrentCell(row, col);
         
-    }
-    public String generateBoardString() {
-        return null;
-    }
-    private int calculateAliveNeighborsOfCurrentCell(int row, int col) {
-        return 0;
-    }
-    private static boolean calculateNextStateOfCell(boolean[][] board, int row, int col) {
-        return false;
+        if (numAlive == OVERCROWD && !cells[row][col].isAlive()) {
+            return true;
+        } else if (numAlive >= EXPOSURE && numAlive <= OVERCROWD && cells[row][col].isAlive()) {
+            return true;
+        }  else {
+            return false;
+        }    
     } 
-    public void nextState() {
+    public void calculateNextState() {
+        Cell[][] newState = new Cell[height][width];
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                
+                boolean aliveOrDead = calculateNextStateOfCell(row, col);
+                
+                newState[row][col].setAlive(aliveOrDead);
+                
+            }   
+        }
+        
+        cells = newState;
         
     }
     private static boolean gabbaRayCalculateNextStateOfCell(int row, int col) {
@@ -68,6 +106,10 @@ public class Board {
     }
     private Board meteorStrike() {
         return this;
+    }
+    
+    public String generateBoardString() {
+        return null;
     }
     
 }
